b61774934ede5ab0c938073080576760b0ee03ff
Mohammad Robaitul Islam Bhuiyan <robayetcuet11@gmail.com>
2024-12-04 22:57:27 +0600

    Update tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,28 +1,2 @@
 #!/bin/bash
-
-# Define the path to the Python script
-PYTHON_SCRIPT_PATH="pipeline.py"
-
-# Execute the data pipeline script
-echo "Running the data pipeline script..."
-python $PYTHON_SCRIPT_PATH
-
-# Check if the SQLite database file exists
-DB_FILE="merged_dataset.db"
-if [ -f "$DB_FILE" ]; then
-    echo "$DB_FILE exists."
-else
-    echo "Error: $DB_FILE does not exist."
-    exit 1
-fi
-
-# Check if the CSV file exists
-CSV_FILE="merged_dataset.csv"
-if [ -f "$CSV_FILE" ]; then
-    echo "$CSV_FILE exists."
-else
-    echo "Error: $CSV_FILE does not exist."
-    exit 1
-fi
-
-echo "All tests passed successfully!"
+python ./project/tests.py
